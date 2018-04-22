0d4cbdb19aba3476c8d0d9ebef836593cacffff5
Prem4u <prem.max83@gmail.com>
2018-04-22 15:18:38 +0530

    forms commit

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'events', component: EventsComponent},
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: 'special', component: SpecialEventsComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
